841ded8176e0976f4629506fe3b936c8f1e1f75a
DimpyRed <davidh62012@gmail.com>
2020-05-31 13:51:31 -0700

    added 'crate::' to it

--- common/src/worldgen.rs
+++ common/src/worldgen.rs
@@ -1,7 +1,7 @@
 use rand::{distributions::Uniform, Rng, SeedableRng};
 
 use crate::sim::{DualGraph, VoxelData};
-use {
+use crate::{
     dodeca::{Side, Vertex},
     graph::NodeId,
     world::Material,
